56c425498d08955bfc8bc2ff0c49f2b7bb841d9a
Your Name <you@example.com>
2023-10-11 23:25:35 +0700

    commit lan 2

---
+++ src/main/java/com/mycompany/chamcong/ChamCong.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chamcong;
+
+/**
+ *
+ * @author vannh
+ */
+public class ChamCong {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Nhat Tai ");
+    }
+}
